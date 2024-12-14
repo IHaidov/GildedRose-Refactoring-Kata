0c036dca6da469fb006a22f6028e0419aa18e86c
ivan.haidov <ivan.haidov@sprinterra.com>
2024-12-14 11:09:05 +0100

    complete update_conjured and _update_backstage_passes methods

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -3,6 +3,8 @@
 class GildedRose(object):
     MAX_QUALITY = 50
     MAX_CONJURED_QUALITY = 80
+    BACKSTAGE_TEN_DAYS = 10
+    BACKSTAGE_FIVE_DAYS = 5
 
     def __init__(self, items):
         self.items = items
@@ -26,10 +28,17 @@ class GildedRose(object):
         item.sell_in -= 1
         if item.sell_in < 0 and item.quality < self.MAX_QUALITY:
             item.quality += 1
-            
-    
-    def update_backstage_passes(item):
-        return
+
+    def _update_backstage_passes(self, item):
+        if item.quality < self.MAX_QUALITY:
+            item.quality += 1
+            if item.sell_in <= self.BACKSTAGE_TEN_DAYS:
+                item.quality += 1
+            if item.sell_in <= self.BACKSTAGE_FIVE_DAYS:
+                item.quality += 1
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            item.quality = 0
     
     def update_sulfuras(item):
         return
@@ -43,7 +52,12 @@ class GildedRose(object):
         item.sell_in -= 1
 
     def update_conjured(item):
-        return
+        if item.quality > 0:
+            item.quality -= 2
+            if item.sell_in <= 0:
+                item.quality -= 2
+        item.quality = max(0, item.quality)
+        item.sell_in -= 1
     
 
 class Item:
