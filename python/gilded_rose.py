77ca1977d3a4bbdcbfe1de979ba17743b4d5ec13
andreistaravoitau <andrei.v.staravoitau@gmail.com>
2024-12-14 02:25:38 +0100

    complete update_aged_brie and update_normal_item functions

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -9,36 +9,24 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+            if "Aged Brie" in item.name:
+                self.update_aged_brie(item)
+            elif "Backstage passes" in item.name:
+                self.update_backstage_passes(item)
+            elif "Sulfuras" in item.name:
+                self.update_sulfuras(item)
+            elif "Conjured" in item.name:
+                self.update_conjured(item)
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                self.update_normal_item(item)
 
-    def update_aged_brie(item):
-        return
+    def update_aged_brie(self, item):
+        if item.quality < self.MAX_QUALITY:
+            item.quality += 1
+        item.sell_in -= 1
+        if item.sell_in < 0 and item.quality < self.MAX_QUALITY:
+            item.quality += 1
+            
     
     def update_backstage_passes(item):
         return
@@ -47,7 +35,12 @@ class GildedRose(object):
         return
     
     def update_normal_item(item):
-        return
+        if item.quality > 0:
+            item.quality -= 1
+            if item.sell_in <= 0:
+                item.quality -= 1
+        item.quality = max(0, item.quality)
+        item.sell_in -= 1
 
     def update_conjured(item):
         return
